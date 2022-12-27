620133ee4570f470c1b11f099c23a4d940a06482
Anton Guryanov <guryanow@gmail.com>
2022-12-27 15:07:56 +0300

    init

---
+++ src/main/java/com/guryanov/Main.java
@@ -0,0 +1,18 @@
+package com.guryanov;
+
+import java.util.Arrays;
+public class Main {
+    public static void main(String[] args) {
+        int[] ArrayDigits = {0,1,2,3,4,5,6,7,8,9};
+
+        int first = ArrayDigits[0];
+        int last = ArrayDigits[ArrayDigits.length-1];
+
+        ArrayDigits[0] = last;
+        ArrayDigits[ArrayDigits.length-1] = first;
+
+        System.out.println(first);
+        System.out.println(last);
+        System.out.println(Arrays.toString(ArrayDigits));
+    }
+}
